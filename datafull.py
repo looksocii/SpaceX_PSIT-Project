cfceb41675d9ff7c2b3120bb4ceb67f0474e1536
Puntakarn Kaewsanghan <44793437+61070139@users.noreply.github.com>
2018-11-19 15:22:25 +0700

    Add files via upload

---
+++ datafull.py
@@ -0,0 +1,8 @@
+def datafull():
+    import pandas as pd
+
+    datafull = pd.read_csv('AccidentDataset.csv')
+    datafull = pd.read_csv('AccidentDataset.csv', encoding = "ISO-8859-1")
+    print(datafull)
+
+datafull()
