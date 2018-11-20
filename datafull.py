9a98b3c3e827684fbc0a6dd1ddadef6bff0ae4f6
Puntakarn Kaewsanghan <44793437+61070139@users.noreply.github.com>
2018-11-21 00:50:50 +0700

    Delete datafull.py

--- datafull.py
+++
@@ -1,8 +0,0 @@
-def datafull():
-    import pandas as pd
-
-    datafull = pd.read_csv('AccidentDataset.csv')
-    datafull = pd.read_csv('AccidentDataset.csv', encoding = "ISO-8859-1")
-    print(datafull)
-
-datafull()
